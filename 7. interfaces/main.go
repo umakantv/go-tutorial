e68bc00a8cdb35b7eafda4dcb934163dab9b2d88
Umakant Vashishtha <41837826+umakantv@users.noreply.github.com>
2022-04-17 00:38:35 +0530

    Add code

--- 7. interfaces/main.go
+++ 7. interfaces/main.go
@@ -1,21 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
 func main() {
 
 	Bot()
-	// resp, err := http.Get("http://google.com")
+	resp, err := http.Get("http://google.com")
 
-	// if err != nil {
-	// 	fmt.Println("Error:", err.Error())
-	// 	os.Exit(1)
-	// }
+	if err != nil {
+		fmt.Println("Error:", err.Error())
+		os.Exit(1)
+	}
 
-	// bs := make([]byte, 120)
+	bs := make([]byte, 120)
 
-	// readBytes, _ := resp.Body.Read(bs)
+	readBytes, _ := resp.Body.Read(bs)
 
-	// fmt.Println(readBytes, string(bs))
+	fmt.Println(readBytes, string(bs))
 
-	// io.Copy(os.Stdout, resp.Body)
+	io.Copy(os.Stdout, resp.Body)
 
 }
