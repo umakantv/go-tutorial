42c13edbec36f4dc2d94218080d5ad987dd85c36
Umakant Vashishtha <41837826+umakantv@users.noreply.github.com>
2022-04-16 23:40:44 +0530

    Using Read function from Read Interface

---
+++ 7. interfaces/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	resp, err := http.Get("http://google.com")
+
+	if err != nil {
+		fmt.Println("Error:", err.Error())
+		os.Exit(1)
+	}
+
+	bs := make([]byte, 120)
+
+	readBytes, _ := resp.Body.Read(bs)
+
+	fmt.Println(readBytes, string(bs))
+
+	// io.Copy(os.Stdout, resp.Body)
+
+}
