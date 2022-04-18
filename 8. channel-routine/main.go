db42ab82c5db59ccaaed0205aba09bbf33f56564
Umakant Vashishtha <41837826+umakantv@users.noreply.github.com>
2022-04-18 14:31:42 +0530

    Some observations with routines

--- 8. channel-routine/main.go
+++ 8. channel-routine/main.go
@@ -21,12 +21,16 @@ func main() {
 		// Create child go routines
 		go checkLink(link, c)
 	}
-	log.Println("Finished creating all routines")
+	for i := 0; i < 10; i++ {
+		log.Println(`Finished creating all routines.`)
+	}
 
-	// This is a blocking request for a channel
-	// It will wait till it receives one message from the channel
-	// If there is no message, it will block the program
-	fmt.Println("Message from channel", <-c)
+	for i := 0; i < len(links); i++ {
+		// This is a blocking request for a channel
+		// It will wait till it receives one message from the channel
+		// If there is no message, it will block the program
+		fmt.Println("Message from channel", <-c)
+	}
 
 }
 
@@ -42,32 +46,148 @@ func checkLink(link string, c chan string) {
 	}
 
 	log.Println(link, "is up")
-	c <- link + " is up"
+	c <- link + " is up and working."
+
+	for i := 0; i < 10; i++ {
+		// log.Println(`Finished creating all routines.`)
+		log.Println("Finished testing the link", link)
+	}
 }
 
 /*
 The output at this point is the following:
 
 ----------
-
-2022/04/18 13:30:45 Finished creating all routines
-2022/04/18 13:30:45 Checking http://google.com
-2022/04/18 13:30:45 Checking http://stackoverflow.com
-2022/04/18 13:30:45 Checking http://facebook.com
-2022/04/18 13:30:45 Checking http://amazon.com
-2022/04/18 13:30:45 Checking http://github.com
-2022/04/18 13:30:46 http://google.com is up
-Message from channel http://google.com is up
-
+2022/04/18 14:25:53 Checking http://google.com
+2022/04/18 14:25:53 Finished creating all routines.
+2022/04/18 14:25:53 Checking http://github.com
+2022/04/18 14:25:53 Checking http://amazon.com
+2022/04/18 14:25:53 Checking http://stackoverflow.com
+2022/04/18 14:25:53 Checking http://facebook.com
+2022/04/18 14:25:53 Finished creating all routines.
+2022/04/18 14:25:53 Finished creating all routines.
+2022/04/18 14:25:53 Finished creating all routines.
+2022/04/18 14:25:53 Finished creating all routines.
+2022/04/18 14:25:53 Finished creating all routines.
+2022/04/18 14:25:53 Finished creating all routines.
+2022/04/18 14:25:53 Finished creating all routines.
+2022/04/18 14:25:53 Finished creating all routines.
+2022/04/18 14:25:53 Finished creating all routines.
+2022/04/18 14:25:54 http://github.com is up
+2022/04/18 14:25:54 Finished testing the link http://github.com
+2022/04/18 14:25:54 Finished testing the link http://github.com
+2022/04/18 14:25:54 Finished testing the link http://github.com
+2022/04/18 14:25:54 Finished testing the link http://github.com
+2022/04/18 14:25:54 Finished testing the link http://github.com
+2022/04/18 14:25:54 Finished testing the link http://github.com
+2022/04/18 14:25:54 Finished testing the link http://github.com
+2022/04/18 14:25:54 Finished testing the link http://github.com
+Message from channel http://github.com is up and working.
+2022/04/18 14:25:54 Finished testing the link http://github.com
+2022/04/18 14:25:54 Finished testing the link http://github.com
+2022/04/18 14:25:54 http://google.com is up
+2022/04/18 14:25:54 Finished testing the link http://google.com
+2022/04/18 14:25:54 Finished testing the link http://google.com
+2022/04/18 14:25:54 Finished testing the link http://google.com
+2022/04/18 14:25:54 Finished testing the link http://google.com
+2022/04/18 14:25:54 Finished testing the link http://google.com
+2022/04/18 14:25:54 Finished testing the link http://google.com
+2022/04/18 14:25:54 Finished testing the link http://google.com
+2022/04/18 14:25:54 Finished testing the link http://google.com
+2022/04/18 14:25:54 Finished testing the link http://google.com
+2022/04/18 14:25:54 Finished testing the link http://google.com
+Message from channel http://google.com is up and working.
+2022/04/18 14:25:54 http://stackoverflow.com is up
+2022/04/18 14:25:54 Finished testing the link http://stackoverflow.com
+2022/04/18 14:25:54 Finished testing the link http://stackoverflow.com
+2022/04/18 14:25:54 Finished testing the link http://stackoverflow.com
+2022/04/18 14:25:54 Finished testing the link http://stackoverflow.com
+2022/04/18 14:25:54 Finished testing the link http://stackoverflow.com
+umakant.vashishtha@RZP2862 8. channel-routine % go run .
+2022/04/18 14:30:11 Finished creating all routines.
+2022/04/18 14:30:11 Finished creating all routines.
+2022/04/18 14:30:11 Finished creating all routines.
+2022/04/18 14:30:11 Finished creating all routines.
+2022/04/18 14:30:11 Finished creating all routines.
+2022/04/18 14:30:11 Finished creating all routines.
+2022/04/18 14:30:11 Finished creating all routines.
+2022/04/18 14:30:11 Finished creating all routines.
+2022/04/18 14:30:11 Finished creating all routines.
+2022/04/18 14:30:11 Finished creating all routines.
+2022/04/18 14:30:11 Checking http://github.com
+2022/04/18 14:30:11 Checking http://facebook.com
+2022/04/18 14:30:11 Checking http://amazon.com
+2022/04/18 14:30:11 Checking http://stackoverflow.com
+2022/04/18 14:30:11 Checking http://google.com
+2022/04/18 14:30:12 http://github.com is up
+2022/04/18 14:30:12 Finished testing the link http://github.com
+2022/04/18 14:30:12 Finished testing the link http://github.com
+2022/04/18 14:30:12 Finished testing the link http://github.com
+2022/04/18 14:30:12 Finished testing the link http://github.com
+2022/04/18 14:30:12 Finished testing the link http://github.com
+2022/04/18 14:30:12 Finished testing the link http://github.com
+2022/04/18 14:30:12 Finished testing the link http://github.com
+2022/04/18 14:30:12 Finished testing the link http://github.com
+2022/04/18 14:30:12 Finished testing the link http://github.com
+2022/04/18 14:30:12 Finished testing the link http://github.com
+Message from channel http://github.com is up and working.
+2022/04/18 14:30:12 http://google.com is up
+2022/04/18 14:30:12 Finished testing the link http://google.com
+2022/04/18 14:30:12 Finished testing the link http://google.com
+2022/04/18 14:30:12 Finished testing the link http://google.com
+2022/04/18 14:30:12 Finished testing the link http://google.com
+2022/04/18 14:30:12 Finished testing the link http://google.com
+2022/04/18 14:30:12 Finished testing the link http://google.com
+2022/04/18 14:30:12 Finished testing the link http://google.com
+2022/04/18 14:30:12 Finished testing the link http://google.com
+2022/04/18 14:30:12 Finished testing the link http://google.com
+2022/04/18 14:30:12 Finished testing the link http://google.com
+Message from channel http://google.com is up and working.
+2022/04/18 14:30:12 http://stackoverflow.com is up
+2022/04/18 14:30:12 Finished testing the link http://stackoverflow.com
+2022/04/18 14:30:12 Finished testing the link http://stackoverflow.com
+2022/04/18 14:30:12 Finished testing the link http://stackoverflow.com
+2022/04/18 14:30:12 Finished testing the link http://stackoverflow.com
+2022/04/18 14:30:12 Finished testing the link http://stackoverflow.com
+2022/04/18 14:30:12 Finished testing the link http://stackoverflow.com
+2022/04/18 14:30:12 Finished testing the link http://stackoverflow.com
+2022/04/18 14:30:12 Finished testing the link http://stackoverflow.com
+2022/04/18 14:30:12 Finished testing the link http://stackoverflow.com
+2022/04/18 14:30:12 Finished testing the link http://stackoverflow.com
+Message from channel http://stackoverflow.com is up and working.
+2022/04/18 14:30:12 http://facebook.com is up
+2022/04/18 14:30:12 Finished testing the link http://facebook.com
+2022/04/18 14:30:12 Finished testing the link http://facebook.com
+2022/04/18 14:30:12 Finished testing the link http://facebook.com
+2022/04/18 14:30:12 Finished testing the link http://facebook.com
+2022/04/18 14:30:12 Finished testing the link http://facebook.com
+2022/04/18 14:30:12 Finished testing the link http://facebook.com
+2022/04/18 14:30:12 Finished testing the link http://facebook.com
+2022/04/18 14:30:12 Finished testing the link http://facebook.com
+2022/04/18 14:30:12 Finished testing the link http://facebook.com
+2022/04/18 14:30:12 Finished testing the link http://facebook.com
+Message from channel http://facebook.com is up and working.
+2022/04/18 14:30:13 http://amazon.com is up
+2022/04/18 14:30:13 Finished testing the link http://amazon.com
+2022/04/18 14:30:13 Finished testing the link http://amazon.com
+2022/04/18 14:30:13 Finished testing the link http://amazon.com
+2022/04/18 14:30:13 Finished testing the link http://amazon.com
+2022/04/18 14:30:13 Finished testing the link http://amazon.com
+2022/04/18 14:30:13 Finished testing the link http://amazon.com
+2022/04/18 14:30:13 Finished testing the link http://amazon.com
+2022/04/18 14:30:13 Finished testing the link http://amazon.com
+2022/04/18 14:30:13 Finished testing the link http://amazon.com
+Message from channel http://amazon.com is up and working.
+2022/04/18 14:30:13 Finished testing the link http://amazon.com
 ----------
 
-Note how the line 29 is logged before logs from line 34.
+Note how the line 29 is logged before logs from line 34, but that will not always be the case.
+> So we can't rely on Lines 24-26 of Main routine getting executed before Lines 38 from other go routines.
 
-Also check the logs after commenting the line 29
+But we can safely say that since all go routines will have a listener,
+the program will terminate after all go routines each will have sent a message.
 
-----------
+Interesting thing to note here is that:
+And that lines 51-54 for each go routine will be executed before execution goes back to Line 32 on the Main routine.
 
-2022/04/18 13:33:01 Finished creating all routines
-
-----------
 */
