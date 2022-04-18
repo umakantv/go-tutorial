3ffbcb73fcbce0d37491444b4933ceeca20899ca
Umakant Vashishtha <41837826+umakantv@users.noreply.github.com>
2022-04-19 01:44:09 +0530

    Request params, and code refactor

--- 9. rest-api-hello-world/app/app.go
+++ 9. rest-api-hello-world/app/app.go
@@ -3,12 +3,16 @@ package app
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func Start() {
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/hello", hello)
-	mux.HandleFunc("/customers", getAllCustomers)
-	log.Fatal(http.ListenAndServe("localhost:5555", mux))
+	// router := http.NewServeMux()
+	router := mux.NewRouter()
+	router.HandleFunc("/hello", hello)
+	router.HandleFunc("/customers", getAllCustomers)
+	router.HandleFunc("/customers/{customer_id}", getCustomer)
+	log.Fatal(http.ListenAndServe("localhost:5555", router))
 }
--- 9. rest-api-hello-world/app/handler.go
+++ 9. rest-api-hello-world/app/handler.go
@@ -7,17 +7,45 @@ import (
 	"log"
 	"math"
 	"net/http"
+	"strconv"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
 // Customer struct helps to serialize customers data to json using json.Marshal.
 // Remember To Capitalize the First Letter for Keys That You Want to See in Your JSON.
 // This cost me 1 hour: https://yourbasic.org/golang/gotcha-json-marshal-empty/
 type Customer struct {
+	Id      int       `json:"id"`
 	Name    string    `json:"full_name"`
 	City    string    `json:"city"`
 	ZipCode string    `json:"zip_code"`
 	t       complex64 // this will not be converted to json as it is not exported
+}
+
+var customers = []Customer{
+	{
+		1,
+		"Umakant",
+		"Delhi",
+		"110053",
+		1 + 3i,
+	},
+	{
+		2,
+		"Varun",
+		"Delhi",
+		"110053",
+		1 + 3i,
+	},
+	{
+		3,
+		"Vikrant",
+		"Delhi",
+		"110053",
+		1 + 3i,
+	},
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
@@ -28,41 +56,43 @@ func hello(w http.ResponseWriter, r *http.Request) {
 
 func getAllCustomers(w http.ResponseWriter, r *http.Request) {
 	log.Println("GET /customers")
-	customers := []Customer{
-		{
-			"Umakant",
-			"Delhi",
-			"110053",
-			1 + 3i,
-		},
-		{
-			"Varun",
-			"Delhi",
-			"110053",
-			1 + 3i,
-		},
-		{
-			"Vikrant",
-			"Delhi",
-			"110053",
-			1 + 3i,
-		},
-	}
+	writeJsonOrXml(w, r, customers)
+}
 
+func getCustomer(w http.ResponseWriter, r *http.Request) {
+	log.Println("GET /customers")
+
+	id, e := strconv.Atoi((mux.Vars(r)["customer_id"]))
+	if e != nil {
+		log.Panicln("Bad input data for customer id.", e.Error())
+		fmt.Fprint(w, "Bad input data for customer id.")
+		return
+	}
+	for _, customer := range customers {
+		if customer.Id == id {
+			writeJsonOrXml(w, r, customer)
+			return
+		}
+	}
+	fmt.Fprint(w, "Not Found")
+}
+
+func writeJsonOrXml(w http.ResponseWriter, r *http.Request, data any) {
 	var e error
 
 	if r.Header.Get("Content-Type") == "application/xml" {
 		w.Header().Add("Content-Type", "application/xml")
-		e = xml.NewEncoder(w).Encode(customers)
+		e = xml.NewEncoder(w).Encode(data)
 	} else {
 		w.Header().Add("Content-Type", "application/json")
-		e = json.NewEncoder(w).Encode(customers)
+		e = json.NewEncoder(w).Encode(data)
 	}
 
 	if e != nil {
 		// throw error here
 		fmt.Fprint(w, "Server Error: ", e.Error())
 	}
+
 }
 
 // TrimLogWriter implements io.Writer for testing the json that is getting written to the Response.
