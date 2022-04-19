1ad1b362f5460adc3172172e76dd3294bd4b0bb9
Umakant Vashishtha <41837826+umakantv@users.noreply.github.com>
2022-04-19 19:53:26 +0530

    Handle default time zone better

--- 10. assignment-time-api/app/handler.go
+++ 10. assignment-time-api/app/handler.go
@@ -21,10 +21,6 @@ func getCurrentTime(w http.ResponseWriter, r *http.Request) {
 	log.Println("TZ", tz)
 	w.Header().Add("Content-Type", "application/json")
 
-	if tz == "" {
-		tz = "Etc/UTC"
-	}
-
 	tzs := strings.Split(tz, ",")
 
 	current_times := make(map[string]string)
@@ -39,18 +35,15 @@ func getCurrentTime(w http.ResponseWriter, r *http.Request) {
 			log.Println("Error in setting the timezone", e.Error())
 			return
 		}
-		current_times[tz] = time.Now().In(l).Format("2006-01-02 15:04:05 -0700 MST")
-	}
-
-	if len(tzs) > 1 {
-		log.Println("Response", current_times)
-		json.NewEncoder(w).Encode(current_times)
-	} else {
-		res := TimeResponse{
-			CurrentTime: current_times[tz],
+		ts := time.Now().In(l).Format("2006-01-02 15:04:05 -0700 MST")
+		if tz == "" {
+			current_times["current_time"] = ts
+		} else {
+			current_times[tz] = ts
 		}
-
-		json.NewEncoder(w).Encode(res)
 	}
+
+	log.Println("Response", current_times)
+	json.NewEncoder(w).Encode(current_times)
 
 }
